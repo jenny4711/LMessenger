129d27ff2ac6f829bd6967f3312c3c73b92c99d9
jiylee <jjenny4711@gmail.com>
2025-05-21 19:17:28 -0400

    xcode

---
+++ LMessenger/AppDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate.swift
+//  LMessenger
+//
+//  Created by Ji y LEE on 5/21/25.
+//
+import SwiftUI
+import FirebaseCore
+
---
+++ LMessenger/LMessengerApp.swift
@@ -0,0 +1,18 @@
+//
+//  LMessengerApp.swift
+//  LMessenger
+//
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import FirebaseStorage
+@main
+struct LMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
